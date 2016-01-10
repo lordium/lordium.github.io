1d15ef4aa565bc6ebbe1cb5a23be0ffc1beebae4
Arslan Rafique <arslan_rafique@ymail.com>
2016-01-10 23:17:12 +0400

    ADD: front page added

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lordiumApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lordiumApp
+ */
+angular.module('lordiumApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
